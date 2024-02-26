1cfdc5773ba8e60adb64c3b9f82cc97d061052c3
Kasper Navnebeskyttet (KASP789Z) <kasp789z@zbc.dk>
2024-02-26 13:53:36 +0100

    Update

---
+++ ScreenManager/Box.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenManager
+{
+    internal class Box
+    {
+        internal static void Draw(int width, int height, int left, int top, string pHeader, string pFooter, Alignment pHeaderAlignment, Alignment pFooterAlignment)
+        {
+            Object.InsertAt(left, top, (string.Concat(GetPart(BoxBorderPart.TopLeft) + Aligner.Align(pHeader, pHeaderAlignment, width - (GetPart(BoxBorderPart.TopLeft).Length) - (GetPart(BoxBorderPart.TopRight).Length), GetPart(BoxBorderPart.Top)) + GetPart(BoxBorderPart.TopRight) + "\n")));
+
+            for(int i = 1; i < height - 1; i++)
+            {
+                Object.InsertAt(left, top + i, GetPart(BoxBorderPart.Left));
+                Object.InsertAt(left + width - 1, top + i, GetPart(BoxBorderPart.Right));
+            }
+
+            Object.InsertAt(left, top + height - 2, (string.Concat(GetPart(BoxBorderPart.BottomLeft) + Aligner.Align(pHeader, pFooterAlignment, width - (GetPart(BoxBorderPart.BottomLeft).Length) - (GetPart(BoxBorderPart.BottomRight).Length), GetPart(BoxBorderPart.Top)) + GetPart(BoxBorderPart.BottomRight) + "\n")));
+        }
+
+        internal enum BoxBorderPart
+        {
+            TopLeft,
+            Top,
+            TopRight,
+            Left,
+            Right,
+            BottomLeft,
+            Bottom,
+            BottomRight
+        }
+
+        static public string GetPart(BoxBorderPart part)
+        {
+            return part switch
+            {
+                BoxBorderPart.TopLeft => "┌",
+                BoxBorderPart.Top => "─",
+                BoxBorderPart.TopRight => "┐",
+                BoxBorderPart.Left => "│",
+                BoxBorderPart.Right => "│",
+                BoxBorderPart.BottomLeft => "└",
+                BoxBorderPart.Bottom => "─",
+                BoxBorderPart.BottomRight => "┘",
+                _ => throw new InvalidOperationException("Unknown border part."),
+            };
+        }
+    }
+}
---
+++ ScreenManager/Program.cs
@@ -0,0 +1,5 @@
+﻿using ScreenManager;
+
+Box.Draw(180,45,0,0, " Header", " Footer ", Alignment.Center, Alignment.Right);
+Box.Draw(178, 43, 1, 1, "", "Footer 1", Alignment.Center, Alignment.Right);
+
